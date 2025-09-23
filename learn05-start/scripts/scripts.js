9989f5be39b7de0a3289e39180f1e3a46d7c2d21
rGToaster <tonycervantes333@gmail.com>
2025-09-23 00:50:55 -0500

    already did this delete later

---
+++ learn05-start/scripts/scripts.js
@@ -0,0 +1,21 @@
+// Functions, objects, and events
+// Functions, objects, and events
+// Dog objects
+const dog1 = {
+    heading: "Bucket Case",
+    image: "images/black-dog.jpg",
+    caption: "Just hanging out waiting for water",
+    altTag: "Black dog in bucket."
+};
+function chooseDog(dog) {
+    if (dog == 1) {
+        loadMe(dog1, 1) // don't put dog1 in quotes or it will pass a string not the object
+    }
+}
+function loadMe(dog, dogNum) {
+    document.getElementById("heading" + dogNum).innerHTML = dog.heading;
+    document.getElementById("img" + dogNum).src = dog.image;
+    document.getElementById("img" + dogNum).style.display = "block";
+    document.getElementById("p" + dogNum).innerHTML = dog.caption;
+    document.getElementById("img" + dogNum).setAttribute('alt', dog.altTag);
+}
